023da19100583c7e47458ac83c31b7af5130d6b2
Arthur Lehdermann <ArthurLehdermann@gmail.com>
2026-01-28 11:29:49 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from fastapi import FastAPI, UploadFile, File, HTTPException
+from fastapi import FastAPI, UploadFile, File, HTTPException, Depends, status
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
 import numpy as np
 import cv2
 import zxingcpp
+import secrets
+
+security = HTTPBasic()
+
+USERNAME = "bigworks"
+PASSWORD = "1122334455"
 
 app = FastAPI(
     title="QR Reader API",
@@ -9,8 +16,25 @@ app = FastAPI(
     version="1.0.0"
 )
 
+def authenticate(credentials: HTTPBasicCredentials = Depends(security)):
+    correct_username = secrets.compare_digest(credentials.username, USERNAME)
+    correct_password = secrets.compare_digest(credentials.password, PASSWORD)
+
+    if not (correct_username and correct_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Unauthorized",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+
+    return credentials.username
+
+
 @app.post("/api/read-qrcode")
-async def read_qrcode(file: UploadFile = File(...)):
+async def read_qrcode(
+    file: UploadFile = File(...),
+    _: str = Depends(authenticate),
+):
     try:
         contents = await file.read()
 
